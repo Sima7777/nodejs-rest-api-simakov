4662efbdae9be60a457b44c5dd1a9a65cba8d26a
Oleksandr Simakov <simakov.mailbox@gmail.com>
2021-10-24 21:40:30 +0300

    adds sendEmail helper, verify and verificationToken to User model, completes  verify and repeatVerify controllers and respective get and post auth routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,11 @@
-const { Conflict } = require('http-errors')
-
+const { Conflict, NotFound, BadRequest } = require('http-errors')
 const { User } = require('../models')
-
 const jwt = require('jsonwebtoken')
 const { SECRET_KEY } = process.env
-const { BadRequest } = require('http-errors')
 const gravatar = require('gravatar')
+const { generate } = require('shortid')
+const verificationToken = generate()
+const { sendEmail } = require('../helpers')
 
 const signup = async (req, res) => {
   const { email, password } = req.body
@@ -14,13 +14,21 @@ const signup = async (req, res) => {
   if (user) {
     throw new Conflict('Email in use')
   }
-  const newUser = new User({ email })
+  const newUser = new User({ email, verifyToken: verificationToken })
 
   newUser.setPassword(password)
 
   newUser.avatarURL = gravatar.url(email, {}, false)
 
   await newUser.save()
+
+  const emailTemplate = {
+    to: email,
+    subject: 'Подтверждение регистрации',
+    html: `Для подтверждения почты перейдите по ссылке <a href="http://localhost:3000/api/auth/users/verify/${verificationToken}" target="_blank">Подтвердить почту</a>`,
+  }
+
+  await sendEmail(emailTemplate)
 
   res.status(201).json({
     status: '201 Created',
@@ -33,9 +41,16 @@ const signup = async (req, res) => {
 
 const login = async (req, res) => {
   const { email, password } = req.body
-  const user = await User.findOne({ email }, '_id email password subscription')
+  const user = await User.findOne(
+    { email },
+    '_id email password subscription verify',
+  )
   if (!user || !user.comparePassword(password)) {
     throw new BadRequest('Email or password is wrong')
+  }
+
+  if (!user.verify) {
+    throw new BadRequest('Email is not verified')
   }
 
   const { _id, subscription } = user
@@ -79,4 +94,52 @@ const current = async (req, res) => {
   })
 }
 
-module.exports = { signup, login, logout, current }
+const verify = async (req, res) => {
+  const { verificationToken } = req.params
+  const user = await User.findOne({ verificationToken })
+  if (!user) {
+    throw new NotFound('User not found')
+  }
+  await User.findByIdAndUpdate(user._id, {
+    verificationToken: null,
+    verify: true,
+  })
+  res.json({
+    status: '200 OK',
+    message: 'Verification is successful',
+  })
+}
+
+const repeatVerify = async (req, res) => {
+  const { email } = req.body
+
+  if (!email) {
+    res.json({
+      status: '400',
+      message: 'missing required field email',
+    })
+  }
+
+  const user = await User.findOne(
+    { email },
+    '_id email password subscription verify',
+  )
+
+  if (user.verify) {
+    throw new BadRequest('Verification has already been passed')
+  }
+
+  const emailTemplate = {
+    to: email,
+    subject: 'Повторное письмо для подтверждения регистрации',
+    html: `Для подтверждения почты перейдите по ссылке <a href="http://localhost:3000/api/auth/users/verify/${verificationToken}" target="_blank">Подтвердить почту</a>`,
+  }
+
+  await sendEmail(emailTemplate)
+  res.status(200).json({
+    status: '200 OK',
+    message: 'Verification email sent',
+  })
+}
+
+module.exports = { signup, login, logout, current, verify, repeatVerify }
--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,9 @@
 const sendSuccessRes = require('./sendSuccessRes')
 const resize = require('./resize')
+const sendEmail = require('./sendEmail')
 
 module.exports = {
   sendSuccessRes,
   resize,
+  sendEmail,
 }
